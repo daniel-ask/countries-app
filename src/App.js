8da31cde04670d0091ba1a884c49c0f010d0b792
Daniel Ask <cdanielask@gmail.com>
2021-06-23 15:12:50 +1000

    Updates the App component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
 import React from "react";
-import Country from './Country';
-import './App.css';
+import Country from "./Country";
+import "./App.css";
 
 export default class App extends React.Component {
-	constructor(props){
-		super(props)
-		this.state = {data: []};
-	}
+  constructor(props) {
+    super(props);
+    this.state = { data: [], countriesFound: true };
+  }
 
-	async fetchCountries(e){
-		try{
-		const url = e ? `https://restcountries.eu/rest/v2/name/${e.target.value}`	: 'https://restcountries.eu/rest/v2/all';
-		const res = await fetch(url)
-		if(res.status >= 400){
-			throw new Error('Something went wrong')
-		}
-		const data = await res.json();
-		this.setState({ data: data })
-		}catch(err){
-			console.log(err)
-		}
-	}
+  async fetchCountries(e) {
+    try {
+      const url =
+        e && e.target.value
+          ? `https://restcountries.eu/rest/v2/name/${e.target.value}`
+          : "https://restcountries.eu/rest/v2/all";
+			
+      const res = await fetch(url);
+      if (res.status >= 400) {
+        throw new Error("Something went wrong");
+      }
+      const data = await res.json();
+      this.setState({ data: data, countriesFound: true });
+    } catch (err) {
+      console.log(err);
+      this.setState({ countriesFound: false });
+    }
+  }
 
-	 componentDidMount(){
-		 this.fetchCountries();
-	}
+  componentDidMount() {
+    this.fetchCountries();
+  }
 
   render() {
-		const { data } = this.state;
-		console.log(this.state)
+    const { data } = this.state;
     return (
-      <div className='app'>
+      <div className="app">
         <h1>Country App</h1>
-        <input type="text" className='input' onChange={(event) => this.fetchCountries(event) }/>
-				<section className='country-list'>
-					{data.map((country) => {
-						return <Country key={country.name} countryData={country}/>
-					})}
-				</section>
+        <input
+          type="text"
+          className="input"
+          onChange={(e) => this.fetchCountries(e)}
+        />
+        {this.state.countriesFound ? (
+          <section className="country-list">
+            {data.map((country) => {
+              return <Country key={country.name} countryData={country} />;
+            })}
+          </section>
+        ) : (
+          <h1>No Countries found</h1>
+        )}
       </div>
     );
   }
